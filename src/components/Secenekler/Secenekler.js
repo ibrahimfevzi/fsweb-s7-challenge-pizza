8bea54490d1d3556348a6251c5b6d39da1106700
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-04-12 00:51:58 +0300

    son düzeltmeler

--- src/components/Secenekler/Secenekler.js
+++ src/components/Secenekler/Secenekler.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Secenekler = () => {
+  const history = useHistory();
+
+  const handleMain = () => {
+    history.push("/");
+  };
   return (
     <>
-      <h1>SEÇENEKLER</h1>
-      <p>Sayfa hazırlanıyor...</p>
+      <div className="secenekler">
+        {" "}
+        <h1>SEÇENEKLER</h1>
+        <p>Sayfa hazırlanıyor...</p>
+        <button id="aciktim" onClick={handleMain}>
+          ANASAYFAYA DÖN
+        </button>
+      </div>
     </>
   );
 };
--- src/components/Success.js
+++ src/components/Success.js
@@ -13,15 +13,20 @@ const Success = () => {
       <div className="success">
         <div>
           <h1>Teknolojik Yemekler</h1>
-        </div>
-        <p className="lezzet">lezzetin yolda</p>
-        <p className="KodAciktirir">
-          TEBRİKLER <br /> SİPARİŞİNİZ ALINDI!
           <br />
-        </p>
-        <button id="aciktim" onClick={handleMain}>
-          ANASAYFAYA DÖN
-        </button>
+          <br />
+          <br />
+        </div>
+        <div className="alt-kisim">
+          <p className="lezzet">lezzetin yolda</p>
+          <p className="KodAciktirir">
+            TEBRİKLER <br /> SİPARİŞİNİZ ALINDI!
+            <br />
+          </p>
+          <button id="aciktim" onClick={handleMain}>
+            ANASAYFAYA DÖN
+          </button>
+        </div>
       </div>
     </>
   );
